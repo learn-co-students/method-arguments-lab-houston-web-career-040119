5d87247b3bd91f12a432ec4e453d329d9b0c6fd3
Jusitn Avioli <justin.avioli@gmail.com>
2019-03-20 15:50:01 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,9 @@
+#Takes in an argument of a name and puts out a phrase with that name using string interpolation
+def introduction(name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+#Take in two arguments, a name and a language and puts out a phrase using those arguments
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
